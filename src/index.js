571ddf6a4803548f83c8f283adf00f5988220b0b
minhyukwoo <minhyukwoo.dev@gmail.com>
2022-08-11 00:39:08 +0900

    Update files fit to chapter 09

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import CommentList from "./chapter_05/CommentList";
 import NotificationList from "./chapter_06/NotificationList";
 import Accommodate from "./chapter_07/Accommodate";
 import ConfirmButton from "./chapter_08/ConfirmButton";
+import LandingPage from "./chapter_09/LandingPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -23,7 +24,8 @@ root.render(
     {/* <CommentList/> */}
     {/* <NotificationList></NotificationList> */}
     {/* <Accommodate></Accommodate> */}
-    <ConfirmButton></ConfirmButton>
+    {/* <ConfirmButton></ConfirmButton> */}
+    <LandingPage></LandingPage>
   </React.StrictMode>
 );
 

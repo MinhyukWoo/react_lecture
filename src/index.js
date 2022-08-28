df1136c346d8db4fa7c3af5b0e857e1b9331717a
minhyukwoo <minhyukwoo.dev@gmail.com>
2022-08-29 00:34:51 +0900

    Update files fit to chapter 15

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ import SignUp from "./chapter_11/SignUp";
 import Calculator from "./chapter_12/Calculator";
 import ProfileCard from "./chapter_13/ProfileCard";
 import DarkOrLight from "./chapter_14/DarkOrLight";
+import Blocks from "./chapter_15/Block";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -35,7 +36,8 @@ root.render(
     {/* <SignUp></SignUp> */}
     {/* <Calculator /> */}
     {/* <ProfileCard /> */}
-    <DarkOrLight></DarkOrLight>
+    {/* <DarkOrLight></DarkOrLight> */}
+    <Blocks></Blocks>
   </React.StrictMode>
 );
 
